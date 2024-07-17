f6986b3344945f2c3f498493ae4b9b67f1ca8b51
Matěj Štolfa <matej@inuits.eu>
2024-07-17 22:10:29 +0200

    add delete endpoint

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import cors from 'cors';
 import axios from 'axios';
+import cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
 
 const app = express();
 const port = 14123;
@@ -93,6 +95,40 @@ app.put('/api/data/matches/:id', (req, res) => {
   });
 });
 
+app.delete('/api/data/matches/:id', (req, res) => {
+  const id = req.params.id;
+  fs.readFile(matchesUrl, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+      return;
+    }
+
+    let matches;
+    try {
+      matches = JSON.parse(data);
+      if (!Array.isArray(matches)) {
+        matches = [];
+      }
+    } catch (parseErr) {
+      matches = [];
+    }
+
+    const updatedMatches = matches.filter((match) => match.id !== id);
+
+    fs.writeFile(
+      matchesUrl,
+      JSON.stringify(updatedMatches, null, 2),
+      (writeErr) => {
+        if (writeErr) {
+          res.status(500).send('Error writing file');
+          return;
+        }
+        res.send({ matches: updatedMatches });
+      }
+    );
+  });
+});
+
 app.get('/api/sofascore/search-match', async (req, res) => {
   try {
     const response = await axios.get(
@@ -109,5 +145,51 @@ app.get('/api/sofascore/search-match', async (req, res) => {
     res
       .status(500)
       .send('An error occurred while fetching data from Sofascore');
+  }
+});
+
+async function fetchPlayerData(playerName) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const url = `https://www.tennisabstract.com/cgi-bin/wplayer.cgi?p=${playerName}`;
+
+    await page.goto(url, { waitUntil: 'networkidle0' }); // Wait until page is fully loaded
+
+    const html = await page.content();
+    await browser.close();
+
+    const $ = cheerio.load(html);
+
+    // Find the year-end rankings table
+    const table = $('#year-end-rankings tbody');
+    const currentYearRow = table.find('tr:first-child');
+    const elo = parseFloat(
+      currentYearRow.find('td:nth-child(5)').text().trim()
+    );
+
+    return { elo };
+  } catch (error) {
+    console.error('Error fetching player data:', error);
+    throw error;
+  }
+}
+
+app.get('/api/elo', async (req, res) => {
+  const { player1, player2 } = req.query;
+
+  try {
+    const player1Data = await fetchPlayerData(player1);
+    const player2Data = await fetchPlayerData(player2);
+    console.log('DATA');
+    console.log(player1Data);
+    console.log('=====');
+    res.json({
+      player1: { name: player1, elo: player1Data.elo },
+      player2: { name: player2, elo: player2Data.elo },
+    });
+  } catch (error) {
+    console.error('Error fetching and parsing data:', error);
+    res.status(500).send('Error fetching data');
   }
 });
